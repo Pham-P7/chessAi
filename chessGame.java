56ddc174a80b78d0823b90622e17a233c3acd74b
Phat Pham <101293853+Pham-P7@users.noreply.github.com>
2023-09-15 10:48:04 -0500

    Add files via upload

--- chessGame.java
+++ chessGame.java
@@ -1,27 +1,34 @@
 public class chessGame {
     public static void main(String... args){
         String Board[][] = startGame();
-        for(int i = 0;i < 8;i++){
-            System.out.println(Board[i][1]);
+        for(int i = 0; i < 8;i++){
+            for(int x = 0; x < 8; x++){
+                System.out.println("|" + Board[i][x] + "|");
+            }
         }
 
     }
     public static String[][] startGame(){
-        String[] backRow = {"Rook","Knight","Bishop","Queen","King","Bishop","Knight","Rook"};
+        String[] Pieces = {"Rk","Kt","Bp","Qn","Kg","Bp","Kt","Rk","Pn"};
         String[][] Board = new String[8][8];
         for(int row = 0;row < 8;row++){
             for(int col = 0; col < 8; col++){
                 if(row == 0 || row == 7){
-                    Board[row][col] = backRow[col];
+                        if(row == 0)
+                            Board[row][col] = "\u001B[30m" + Pieces[col] + "\u001B[0m";
+                        else if(row == 7)
+                            Board[row][col] = "\u001B[37m" + Pieces[col] + "\u001B[0m";
                 }
                 else if(row == 1 || row == 6){
-                    Board[row][col] = "Pawn";
+                    if(row == 1)
+                        Board[row][col] = "\u001B[30 " + "P" + Pieces[8] + "\u001B[0m";
+                    else if(row == 6)
+                        Board[row][col] = "\u001B[37 " + "P" + Pieces[8] + "\u001B[0m";
                 }
-                else{
+                else
                     Board[row][col] = "  ";
-                }
             }
         }
-        return Board;
+            return Board;
     }
 }
