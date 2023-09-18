14e23333445f134dc612dbf2eb2e411e02ec0408
Phat Pham <101293853+Pham-P7@users.noreply.github.com>
2023-09-18 11:00:06 -0500

    Update chessGame.java

--- chessGame.java
+++ chessGame.java
@@ -1,22 +1,46 @@
-import java.io.bufferedReader; //import my input functions
+import java.io.BufferedReader; //import my input functions
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class chessGame {
-    public static void main(String... args){
-        bufferedReader input = new bufferedReader();
+    String Board[][];
+    String playerColor, aiColor;
+    public static void main(String... args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("What piece color do you want? White or Black, White goes first");
-        chessGame game = new chessGame(input.readLine());
+        chessGame game = new chessGame(input.readLine()); //creates a game object of the chessGame class
+        // System.out.println(game.whatColor("11"));
+        game.printBoard();
+        
 
     }
-
-    public chessGame(String color){
-        String Board[][] = this.startGame();
-        String playerColor;
-        String aiColor
+    public int whatColor(String index){ //return 1 if white, 2 if black, -1 if not valid index
+        if(index.length() > 2){
+            return -1;
+        }
+        int firstIndex = Integer.valueOf(index.charAt(0));
+        int secondIndex = Integer.valueOf(index.charAt(1));
+        if(this.Board[firstIndex][secondIndex].contains("\u001B[30m"))
+            return 1;
+        else if(this.Board[firstIndex][secondIndex].contains("\u001B[37m]"))
+            return 2;
+        return -1;
+    }
+    public void printBoard(){
+        for(int i = 0;i < 8; i++){
+            System.out.println("-------------------------");
+            System.out.println("|" + String.join("|",this.Board[i]) + "|");
+        }
+        System.out.println("-------------------------");
+    }
+    public chessGame(String color){ //creates the board and assigns the colors
+        Board = this.startGame();
         if(color.equalsIgnoreCase("white") || color.equalsIgnoreCase("black")){
             playerColor = color;
             aiColor = color.equalsIgnoreCase("white") ? "black" : "white";
         }
     }
-    public String[][] startGame(){
+
+    public String[][] startGame(){ //makes an empty game board
         String[] Pieces = {"Rk","Kt","Bp","Qn","Kg","Bp","Kt","Rk","Pn"};
         String[][] Board = new String[8][8];
         for(int row = 0;row < 8;row++){
@@ -35,6 +59,12 @@ public class chessGame {
                 }
                 else
                     Board[row][col] = "  ";
+            }
+        }
+            return Board;
+    }
+}
+
             }
         }
             return Board;
