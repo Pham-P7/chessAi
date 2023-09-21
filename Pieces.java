87a90f1127081511167269f40fafb35f425f79f7
Phat Pham <101293853+Pham-P7@users.noreply.github.com>
2023-09-21 11:00:44 -0500

    Add files via upload

--- Pieces.java
+++ Pieces.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+
 public class Pieces {
 final String redCode = "\u001B[31m"; //all required variables
 final String whiteCode = "\u001B[37m";
 final String resetCode = "\u001B[0m";
 String color;
 String type;
+ArrayList<moves> move_values;
+class moves{
+    String move;
+    int values;
+    public moves(String move){
+        this.move = move;
+    }
+}
 public Pieces(String piece){
     if(piece.contains(redCode))
         this.color = "red";
@@ -11,6 +21,6 @@ public Pieces(String piece){
         this.color = "white";
     else
         this.color = null;
-    
 }
+
 }
